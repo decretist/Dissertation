5fd9cf400d69f0f344eaa1c1acd62e90ea5e9f65
Paul Evans <decretist@gmail.com>
2020-03-06 10:55:42 -0800

    Improved 3-way comparison
    Based on input from Ben Shifflett and Programming Historians tutorial,
    this version plots 'in' and 'non' frequency z-scores for subcorpora
    A (first-recension dicta excluding de Pen.), B (second-recension
    dicta excluding de Pen.) and C (everything else).

--- Chapter4/bin/visualize3.py
+++ Chapter4/bin/visualize3.py
@@ -9,144 +9,119 @@ import math
 import matplotlib.pyplot as pp
 import statistics
 
-def pstdev(data, **kwargs):
-    '''Temporary replacement for statistics.pstdev()'''
-    mu = None
-    if 'mu' in kwargs: mu = kwargs['mu'] # type check: int, float, or None
-    if mu == None: mu = statistics.mean(data)
-    sum = 0
-    for i in range(len(data)):
-        sum += (data[i] - mu) ** 2
-    return(math.sqrt(sum / len(data)))
-
 def stats(word, data):
-    frequency_mean = (data[word][0] / data['all'][0]) * 1000
-    frequency_r1 = (data[word][1] / data['all'][1]) * 1000
-    frequency_r2 = (data[word][2] / data ['all'][2]) * 1000
-    frequency_dP = (data[word][3] / data ['all'][3]) * 1000
-    frequency_values = [frequency_r1, frequency_r2]
-    standard_deviation = pstdev(frequency_values, mu=frequency_mean)
+    frequency_A = (data[word][1] / data['all'][1]) * 1000
+    frequency_B = (data[word][2] / data ['all'][2]) * 1000
+    frequency_C = (data[word][3] / data ['all'][3]) * 1000
+    frequency_values = [frequency_A, frequency_B, frequency_C]
+    frequency_mean = statistics.mean(frequency_values)
+    standard_deviation = statistics.stdev(frequency_values)
     filler = f'occurrences of \'{word}\' per 1,000 words'
-    print(f'{frequency_r1:7.4f} {filler} (R1)')
-    print(f'{frequency_r2:7.4f} {filler} (R2)')
-    print(f'{frequency_dP:7.4f} {filler} (de Pen.)')
+    print(f'{frequency_A:7.4f} {filler} (subcorpus A)')
+    print(f'{frequency_B:7.4f} {filler} (subcorpus B)')
+    print(f'{frequency_C:7.4f} {filler} (subcorpus C)')
     print(f'{frequency_mean:7.4f} {filler} (mean)')
     print(f'{standard_deviation:7.4f} {filler} (standard deviation)')
-    percentage(word, 'R1', frequency_r1, frequency_mean)
-    percentage(word, 'R2', frequency_r2, frequency_mean)
-    percentage(word, 'de Pen.', frequency_dP, frequency_mean)
-    return (frequency_r1, frequency_r2, frequency_dP, frequency_mean, standard_deviation)
+    return (frequency_A, frequency_B, frequency_C, frequency_mean, standard_deviation)
 
 def f_plot(x_word, x_frequencies, y_word, y_frequencies):
     # frequency view
-    value_x_r1,value_x_r2,value_x_dP,frequency_x_mean,standard_deviation_x = x_frequencies
-    value_y_r1,value_y_r2,value_y_dP,frequency_y_mean,standard_deviation_y = y_frequencies
-    pp.axis([frequency_x_mean - 5 * standard_deviation_x,
-        frequency_x_mean + 5 * standard_deviation_x,
-        frequency_y_mean - 5 * standard_deviation_y,
-        frequency_y_mean + 5 * standard_deviation_y])
+    value_x_A,value_x_B,value_x_C,frequency_x_mean,standard_deviation_x = x_frequencies
+    value_y_A,value_y_B,value_y_C,frequency_y_mean,standard_deviation_y = y_frequencies
+    pp.axis([frequency_x_mean - 2 * standard_deviation_x,
+        frequency_x_mean + 2 * standard_deviation_x,
+        frequency_y_mean - 2 * standard_deviation_y,
+        frequency_y_mean + 2 * standard_deviation_y])
     pp.axhline(frequency_y_mean,linestyle='dashed')
     pp.axvline(frequency_x_mean, linestyle='dashed')
     pp.xlabel(f'frequency of occurrence of \'{x_word}\' per 1,000 words')
     pp.ylabel(f'frequency of occurrence of \'{y_word}\' per 1,000 words')
     # common
     pp.annotate(
-        f'R1 ({value_x_r1:.3f}, {value_y_r1:.3f})',
-        (value_x_r1, value_y_r1),
+        f'subcorpus A ({value_x_A:.3f}, {value_y_A:.3f})',
+        (value_x_A, value_y_A),
         textcoords="offset points",
         xytext=(0,10),
         ha='center'
     )
     pp.annotate(
-        f'R2 ({value_x_r2:.3f}, {value_y_r2:.3f})',
-        (value_x_r2, value_y_r2),
+        f'subcorpus B ({value_x_B:.3f}, {value_y_B:.3f})',
+        (value_x_B, value_y_B),
         textcoords="offset points",
         xytext=(0,10),
         ha='center'
     )
     pp.annotate(
-        f'de Pen. ({value_x_dP:.3f}, {value_y_dP:.3f})',
-        (value_x_dP, value_y_dP),
+        f'subcorpus C ({value_x_C:.3f}, {value_y_C:.3f})',
+        (value_x_C, value_y_C),
         textcoords="offset points",
         xytext=(0,10),
         ha='center'
     )
-    x_values = [value_x_r1, value_x_r2, value_x_dP]
-    y_values = [value_y_r1, value_y_r2, value_y_dP]
+    x_values = [value_x_A, value_x_B, value_x_C]
+    y_values = [value_y_A, value_y_B, value_y_C]
     pp.scatter(x_values, y_values)
     pp.title('(frequency view)')
-    # pp.savefig('../PNGs/Figure_0_frequency')
+    # pp.savefig('../PNGs/Figure_ABC_frequency')
     pp.gcf().canvas.set_window_title('Figure 0')
     pp.show()
 
 def z_plot(x_word, x_frequencies, y_word, y_frequencies):
     # z-score view
-    frequency_x_r1,frequency_x_r2,frequency_x_dP,frequency_x_mean,standard_deviation_x = x_frequencies
-    frequency_y_r1,frequency_y_r2,frequency_y_dP,frequency_y_mean,standard_deviation_y = y_frequencies
-    value_x_r1 = (frequency_x_r1 - frequency_x_mean) / standard_deviation_x
-    value_y_r1 = (frequency_y_r1 - frequency_y_mean) / standard_deviation_y
-    value_x_r2 = (frequency_x_r2 - frequency_x_mean) / standard_deviation_x
-    value_y_r2 = (frequency_y_r2 - frequency_y_mean) / standard_deviation_y
-    value_x_dP = (frequency_x_dP - frequency_x_mean) / standard_deviation_x
-    value_y_dP = (frequency_y_dP - frequency_y_mean) / standard_deviation_y
-    pp.axis([-5, 5, -5, 5])
+    frequency_x_A,frequency_x_B,frequency_x_C,frequency_x_mean,standard_deviation_x = x_frequencies
+    frequency_y_A,frequency_y_B,frequency_y_C,frequency_y_mean,standard_deviation_y = y_frequencies
+    value_x_A = (frequency_x_A - frequency_x_mean) / standard_deviation_x
+    value_y_A = (frequency_y_A - frequency_y_mean) / standard_deviation_y
+    value_x_B = (frequency_x_B - frequency_x_mean) / standard_deviation_x
+    value_y_B = (frequency_y_B - frequency_y_mean) / standard_deviation_y
+    value_x_C = (frequency_x_C - frequency_x_mean) / standard_deviation_x
+    value_y_C = (frequency_y_C - frequency_y_mean) / standard_deviation_y
+    pp.axis([-2, 2, -2, 2])
     pp.axhline(linestyle='dashed')
     pp.axvline(linestyle='dashed')
-    pp.xticks([-2.5, 0, 2.5])
-    pp.yticks([-2.5, 0, 2.5])
+    pp.xticks([-1, 0, 1])
+    pp.yticks([-1, 0, 1])
     pp.xlabel(f'\'{x_word}\'')
     pp.ylabel(f'\'{y_word}\'', rotation='horizontal')
     # common
     pp.annotate(
-        f'R1 ({value_x_r1:.3f}, {value_y_r1:.3f})',
-        (value_x_r1, value_y_r1),
+        f'subcorpus A ({value_x_A:.3f}, {value_y_A:.3f})',
+        (value_x_A, value_y_A),
         textcoords="offset points",
         xytext=(0,10),
         ha='center'
     )
     pp.annotate(
-        f'R2 ({value_x_r2:.3f}, {value_y_r2:.3f})',
-        (value_x_r2, value_y_r2),
+        f'subcorpus B ({value_x_B:.3f}, {value_y_B:.3f})',
+        (value_x_B, value_y_B),
         textcoords="offset points",
         xytext=(0,10),
         ha='center'
     )
     pp.annotate(
-        f'de Pen. ({value_x_dP:.3f}, {value_y_dP:.3f})',
-        (value_x_dP, value_y_dP),
+        f'subcorpus C ({value_x_C:.3f}, {value_y_C:.3f})',
+        (value_x_C, value_y_C),
         textcoords="offset points",
         xytext=(0,10),
         ha='center'
     )
-    x_values = [value_x_r1, value_x_r2, value_x_dP]
-    y_values = [value_y_r1, value_y_r2, value_y_dP]
+    x_values = [value_x_A, value_x_B, value_x_C]
+    y_values = [value_y_A, value_y_B, value_y_C]
     pp.scatter(x_values, y_values)
     pp.title('(z-score view)')
-    # pp.savefig('../PNGs/Figure_0_z-score')
+    # pp.savefig('../PNGs/Figure_ABC_z-score')
     pp.gcf().canvas.set_window_title('Figure 0')
     pp.show()
 
-def percentage(word, title, frequency, mean):
-    percentage = ((frequency - mean) / mean) * 100
-    if percentage > 0: more_or_less = 'more'
-    else: more_or_less = 'less'
-    print(f'\'{word}\' occurs {abs(percentage):.2f}% {more_or_less} frequently in {title} than in mean')
-
 def main():
     frequencies = {
-        # include frequency data for 'de'
-        'all': [84654, 56713, 14255,  9525],
-        'in':  [ 2187,  1450,   411,   232],
+        'all': [84654, 56713, 14255, 13686],
+        'in':  [ 2187,  1450,   411,   326],
         'et':  [ 1968,  1293,   345,   249],
-        'non': [ 1960,  1360,   306,   262],
-        # 'de':  [     ,      ,      ,      ]
+        'non': [ 1960,  1360,   306,   294],
     }
     f_plot('in', stats('in', frequencies), 'non', stats('non', frequencies))
     z_plot('in', stats('in', frequencies), 'non', stats('non', frequencies))
-    f_plot('in', stats('in', frequencies), 'et', stats('et', frequencies))
-    z_plot('in', stats('in', frequencies), 'et', stats('et', frequencies))
-    f_plot('et', stats('et', frequencies), 'non', stats('non', frequencies))
-    z_plot('et', stats('et', frequencies), 'non', stats('non', frequencies))
 
 if __name__ == '__main__':
     main()
